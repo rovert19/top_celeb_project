933b9500a297206ab8cc663a75ceec03c6f5d0b7
Roberto Rodriguez Eduardo <70344371+rovert19@users.noreply.github.com>
2024-12-31 19:17:35 -0500

    Update constants.py

--- airflow/include/constants.py
+++ airflow/include/constants.py
@@ -8,12 +8,7 @@ URL_SERIES_CELEB = "https://api.themoviedb.org/3/person/{}/tv_credits?language=e
 URL_MOVIE_DETAILS = "https://api.themoviedb.org/3/movie/{}?language=en-US"
 URL_SERIES_DETAILS = "https://api.themoviedb.org/3/tv/{}?language=en-US"
 
-# TOKEN_TMDB = Variable.get_variable_from_secrets("TOKEN_TMDB")
 TOKEN_TMDB = os.getenv("TOKEN_TMDB")
-# HEADERS = {
-#     "accept": "application/json",
-#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjkyNGI1OWQ1ZDk5ZjNlNzI0MWVlNmM0NDg3MTgzOCIsIm5iZiI6MTczNDQ3MTYzMS4wNzIsInN1YiI6IjY3NjFlZmNmODg2MzE4MGVhMWZiMzgxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ol1xQKJKafNHy2xgtxOBvn5JvjaUkbHsNBOA2lloEnw"
-# }
 
 HEADERS = {
     "accept": "application/json",
